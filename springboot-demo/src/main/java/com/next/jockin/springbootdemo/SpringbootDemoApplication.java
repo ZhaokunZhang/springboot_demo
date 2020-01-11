07612eca91f765f479a8cd7c61a2c033b2a0f1d6
jockin <3288597810@qq.com>
2020-01-11 13:48:02 +0800

    project init

---
+++ springboot-demo/src/main/java/com/next/jockin/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.next.jockin.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
